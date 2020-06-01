3237e3b54abb60cb1e685254f54fb0e93d2ddb6e
Alan Ensina <alan.ensina@grad.ufsc.br>
2020-06-01 19:40:35 -0300

    Component counter

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -14,29 +14,33 @@ import UsuarioInfo from './components/condicional/UsuarioInfo';
 import DiretaPai from './components/comunicacao/DiretaPai';
 import IndiretaPai from './components/comunicacao/IndiretaPai';
 import Input from './components/formulario/Input';
+import Contador from './components/contador/Contador';
 import './App.css';
 
 export default () => (
   <div className="App">
     <h1>Fundamentos React</h1>
     <div className="Cards">
+      <Card titulo="#12 - Contador" color="#C47F70">
+        <Contador numeroInicial={10} />
+      </Card>
 
-    <Card titulo="#11 - Componente controlado" color="#de9fe3">
+      <Card titulo="#11 - Componente controlado" color="#de9fe3">
         <Input />
       </Card>
 
-    <Card titulo="#10 - Comunicação indireta" color="#84aff5">
+      <Card titulo="#10 - Comunicação indireta" color="#84aff5">
         <IndiretaPai />
       </Card>
 
-    <Card titulo="#09 - Comunicação direta" color="#36bf43">
+      <Card titulo="#09 - Comunicação direta" color="#36bf43">
         <DiretaPai />
       </Card>
 
       <Card titulo="#08 - Renderização condicional" color="#9c947e">
         <ParOuImpar numero={20} />
-        <UsuarioInfo usuario={{nome: 'Alan'}}/>
-        <UsuarioInfo/>
+        <UsuarioInfo usuario={{ nome: 'Alan' }} />
+        <UsuarioInfo />
       </Card>
 
       <Card titulo="#07 - Desafio Repetição" color="#ff455e">
